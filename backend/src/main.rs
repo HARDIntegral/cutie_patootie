3f9aaa05f83c731354e713bf959f605ce8c0d892
HARDIntegral <73722314+HARDIntegral@users.noreply.github.com>
2025-07-03 14:13:55 -0400

    updated port information

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,20 @@
 use actix_files::Files;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Result};
 
-#[get("/api/hello")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().json("Hi babe 💖 from backend!")
+// This handles fallback for SPA routes
+async fn fallback() -> Result<actix_files::NamedFile> {
+    Ok(actix_files::NamedFile::open("./static/index.html")?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = std::env::var("PORT").unwrap_or_else(|_| "10000".to_string());
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(Files::new("/", "static").index_file("index.html"))
+            .service(Files::new("/", "./static").index_file("index.html"))
+            .default_service(web::get().to(fallback))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await
 }
